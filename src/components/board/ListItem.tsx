60d968eced634b533eae793b9a75e511e96ae2e9
Matt Whelan <matt@goodthinc.com>
2026-03-01 18:26:08 -0500

    Add scrollbars to individual list columns

--- src/components/board/ListItem.tsx
+++ src/components/board/ListItem.tsx
@@ -345,7 +345,7 @@ export const ListItem = ({ data, index }: { data: any; index: number }) => {
                     items={data.cards.map((c: any) => c.id)}
                     strategy={verticalListSortingStrategy}
                 >
-                    <ol className="mx-1 px-1 py-0.5 flex flex-col gap-y-2 mt-2 min-h-[2px]">
+                    <ol className="mx-1 px-1 py-0.5 flex flex-col gap-y-2 mt-2 min-h-[2px] max-h-[calc(100vh-14rem)] overflow-y-auto">
                         {data.cards.map((card: any, idx: number) => (
                             <CardItem index={idx} key={card.id} data={card} boardId={data.boardId} />
                         ))}
