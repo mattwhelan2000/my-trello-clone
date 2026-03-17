e5714e282a113644c3070a897bac5a1eb60cde38
Matt Whelan <matt@goodthinc.com>
2026-03-17 15:52:47 -0400

    Hotfix: Removing broken Script Writer port attempt to allow clean build

--- src/actions/save-script/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const SaveScriptSchema = z.object({
-  id: z.string().optional(),
-  workspaceId: z.string(),
-  title: z.string().min(1, "Title is required"),
-  author: z.string().optional(),
-  jsonContent: z.string(), // We send the stringified JSON here to be converted to a blob
-});
--- src/app/script/page.tsx
+++
@@ -1,9 +0,0 @@
-import ScriptEditor from "@/components/Editor";
-
-export default function ScriptPage() {
-  return (
-    <div className="w-full h-screen bg-[#333]">
-      <ScriptEditor />
-    </div>
-  );
-}
