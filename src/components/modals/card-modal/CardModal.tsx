460f4a358d44b114ff854566a04a4344831bb973
Matt Whelan <matt@goodthinc.com>
2026-05-05 11:25:10 -0400

    Consolidate lucide-react imports in CardModal

--- src/components/modals/card-modal/CardModal.tsx
+++ src/components/modals/card-modal/CardModal.tsx
@@ -2,7 +2,7 @@
 
 import { Modal } from "@/components/modals/Modal";
 import { useState, useRef, ElementRef, useEffect } from "react";
-import { AlignLeft, Layout, CheckSquare, Clock, Paperclip, Activity, X, Plus } from "lucide-react";
+import { AlignLeft, Layout, CheckSquare, Clock, Paperclip, Activity, X, Plus, Trash2, Copy, Layers, Share2, MoreHorizontal, Eye, EyeOff, MinusSquare, Maximize2, Pencil, Check } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useAction as useSafeAction } from "next-safe-action/hooks";
 import { updateCard } from "@/actions/update-card";
@@ -27,7 +27,7 @@ import { AttachmentPreview, AttachmentPreviewLarge } from "@/components/ui/Attac
 import { AudioPlayer } from "@/components/ui/AudioPlayer";
 import { detectFileType, getFileTypeLabel } from "@/lib/file-type-utils";
 import Image from "next/image";
-import { Trash2, Copy, Layers, Share2, MoreHorizontal, Eye, EyeOff, MinusSquare, Maximize2, Pencil, Check } from "lucide-react";
+
 import { formatImageUrl } from "@/lib/format-image-url";
 
 interface CardModalProps {
